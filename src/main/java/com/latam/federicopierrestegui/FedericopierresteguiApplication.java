cc6cded36e276f507c4bec617da8f1567e13fab1
pierresteguifederico <144521724+pierresteguifederico@users.noreply.github.com>
2025-01-21 01:16:00 -0300

    123

---
+++ src/main/java/com/latam/federicopierrestegui/FedericopierresteguiApplication.java
@@ -0,0 +1,13 @@
+package com.latam.federicopierrestegui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedericopierresteguiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedericopierresteguiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/latam/federicopierrestegui/FedericopierresteguiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latam.federicopierrestegui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedericopierresteguiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
